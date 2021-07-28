01846de72a207d3e11920500c250c0e201cb2810
Seraphina Tarrant <serif@uw.edu>
2021-07-28 17:12:07 +0100

    working version of part 3

--- lab8.py
+++ lab8.py
@@ -2,6 +2,7 @@ import argparse
 import itertools
 import re
 import json
+import pickle
 
 import numpy as np
 import spacy
@@ -14,12 +15,72 @@ from tqdm import tqdm
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 
+### Globals strings ###
+ONTONOTES_LABELS = [
+    'CARDINAL', 'DATE', 'EVENT', 'FAC', 'GPE', 'LANGUAGE', 'LAW', 'LOC', 'MONEY', 'NORP', 'ORDINAL',
+    'ORG', 'PERCENT', 'PERSON', 'PRODUCT', 'QUANTITY', 'TIME', 'WORK_OF_ART']
+
+ontonotes_json = "data/ontonotes5_reduced.json"
+
+
+# TODO have a longer span of text for the second part of this question
+example_text = "On March 8, 2021, a group of hackers including Kottmann and calling themselves " \
+       "'APT - 69420 Arson Cats' gained 'super admin' rights in the network of Verkada, a " \
+       "cloud-based security camera company, using credentials they found on the public " \
+       "internet. They had access to the network for 36 hours. The group collected about 5 " \
+       "gigabytes of data, including live security camera footage and recordings from more" \
+       " than 150,000 cameras in places like a Tesla factory, a jail in Alabama, a Halifax " \
+       "Health hospital, and residential homes. The group also accessed a list of Verkada " \
+       "customers and the company's private financial information, and gained superuser " \
+       "access to the corporate networks of Cloudflare and Okta through their Verkada cameras."
+
+
+def setup_argparse():
+    p = argparse.ArgumentParser()
+    p.add_argument('--part', choices=['1','2','3','4'], required=True)
+    p.add_argument('--ents', choices=ONTONOTES_LABELS, nargs='+')
+    p.add_argument('--viz_output', default='entity_viz_example.html',
+                   help='Name of output file for the visualisation')
+    p.add_argument('--embed_func', choices=['first', 'last', 'sum', 'max', 'avg'],
+                   help='the type of function to use to combine multiple embeddings into one entity'
+                        'representation')
+    return p.parse_args()
+
+#####
+
+def part_1(args, nlp):
+    doc = nlp(example_text)
+    ent_list, output_file = args.ents, args.viz_output
+    options = {"ents": ent_list} if ent_list else {"ents": ONTONOTES_LABELS}
+
+    html = displacy.render(doc, style="ent", options=options)
+
+    output_path = Path(output_file)
+    output_path.open("w").write(html)
+
+
+def part_2(args, nlp):
+    # These are special characters used by the tokenizer, ignore them
+    special_chars = re.compile("Ġ|<pad>|<s>|</s>")
+    doc = nlp(example_text)
+
+    print("List of Entities:")
+    print(doc.ents)
+
+    print("\nStandard Tokenisation:")
+    print(" ".join([tok.text for tok in doc]))
+
+    print("\nSubword Tokenisation:")
+    subword_string = " ".join([tok for tok in itertools.chain(*doc._.trf_data.wordpieces.strings)])
+    cleaned_subword_string = special_chars.sub("", subword_string).strip()
+
+    print(cleaned_subword_string)
 
 ### This is for Part 3 ###
 class ContextualVectors(Pipe):
     def __init__(self, nlp):
         self._nlp = nlp
-        self.combination_function = "average"
+        self.combination_function = "average" ### modify this here for different versions of part 3
 
     def __call__(self, doc):
         if type(doc) == str:
@@ -56,72 +117,13 @@ class ContextualVectors(Pipe):
 
 
 @Language.factory("trf_vector_hook", assigns=["doc.user_token_hooks"])
-def create_contextual_hook(nlp, name): # TODO how do I pass through the type of hook to make it customisable?
-   return ContextualVectors(nlp)
+def create_contextual_hook(nlp, name):
+    return ContextualVectors(nlp)
 
 
-### Globals until flags are sorted out ###
-ONTONOTES_LABELS = [
-    'CARDINAL', 'DATE', 'EVENT', 'FAC', 'GPE', 'LANGUAGE', 'LAW', 'LOC', 'MONEY', 'NORP', 'ORDINAL',
-    'ORG', 'PERCENT', 'PERSON', 'PRODUCT', 'QUANTITY', 'TIME', 'WORK_OF_ART']
+def part_3(args, nlp):
 
-ontonotes_json = "data/ontonotes5_reduced.json"
-
-nlp = spacy.load('en_core_web_trf')
-nlp.add_pipe("trf_vector_hook", last=True)
-
-# TODO have a longer span of text for the second part of this question
-text = "On March 8, 2021, a group of hackers including Kottmann and calling themselves " \
-       "'APT - 69420 Arson Cats' gained 'super admin' rights in the network of Verkada, a " \
-       "cloud-based security camera company, using credentials they found on the public " \
-       "internet. They had access to the network for 36 hours. The group collected about 5 " \
-       "gigabytes of data, including live security camera footage and recordings from more" \
-       " than 150,000 cameras in places like a Tesla factory, a jail in Alabama, a Halifax " \
-       "Health hospital, and residential homes. The group also accessed a list of Verkada " \
-       "customers and the company's private financial information, and gained superuser " \
-       "access to the corporate networks of Cloudflare and Okta through their Verkada cameras."
-doc = nlp(text)
-
-
-def setup_argparse():
-    p = argparse.ArgumentParser()
-    p.add_argument('--part', choices=['1','2','3','4'], required=True)
-    p.add_argument('--ents', choices=ONTONOTES_LABELS, nargs='+')
-    p.add_argument('--viz_output', default='entity_viz_example.html',
-                   help='Name of output file for the visualisation')
-    p.add_argument('--embed_func', choices=['first', 'last', 'sum', 'max', 'avg'],
-                   help='the type of function to use to combine multiple embeddings into one entity'
-                        'representation')
-    return p.parse_args()
-
-
-def part_1(args):
-    ent_list, output_file = args.ents, args.viz_output
-    options = {"ents": ent_list} if ent_list else {"ents": ONTONOTES_LABELS}
-
-    html = displacy.render(doc, style="ent", options=options)
-
-    output_path = Path(output_file)
-    output_path.open("w").write(html)
-
-
-def part_2(args):
-    # These are special characters used by the tokenizer, ignore them
-    special_chars = re.compile("Ġ|<pad>|<s>|</s>")
-
-    print("List of Entities:")
-    print(doc.ents)
-
-    print("\nStandard Tokenisation:")
-    print(" ".join([tok.text for tok in doc]))
-
-    print("\nSubword Tokenisation:")
-    subword_string = " ".join([tok for tok in itertools.chain(*doc._.trf_data.wordpieces.strings)])
-    cleaned_subword_string = special_chars.sub("",subword_string).strip()
-
-    print(cleaned_subword_string)
-
-def part_3(args):
+    nlp.add_pipe("trf_vector_hook", last=True)
     max_tok = 145  # max tokens per chunk
     def chunks(tokens, n):
         for i in range(0, len(tokens), n):
@@ -147,14 +149,14 @@ def part_3(args):
                 text_chunks = [this_string]
             for c in text_chunks:
                 this_doc = nlp("".join(c))
-                #print(doc)
-                # TODO now have to get the entity that is the offset in order to make it gold label? ONTONOTES json is 'entities': {'DATE': [[177, 186]]}
+
+                # TODO if want gold labels, now have to get the entity that is the offset in order to make it gold label? ONTONOTES json is 'entities': {'DATE': [[177, 186]]}
                 # TODO use start_char and end_char on both?
-                # TODO also deal with token length maximum of 512 (and or 145)
+                # TODO also deal with token length maximum of 512 (and or 145) - one thing exceeds it. Also check strides
                 # for silver labels:
                 for ent in this_doc.ents:
                     try:
-                        if not ent.vector.any(): #TODO this should never happen, sort out error
+                        if not ent.vector.any(): # TODO this happens 4 times in ontonotes, but technically should not with subwords so it's weird
                             continue
                     except:
                         print(f"Error on entity '{ent}' in document: {this_doc}")
@@ -167,8 +169,8 @@ def part_3(args):
     for key in corpus.keys():
         print("{}: {} entities".format(key, len(corpus[key][0])))
 
-    with open("corpus.json", "w") as fout:
-        json.dump(corpus, fout)
+    with open("corpus.pkl", "wb") as fout:
+        pickle.dump(corpus, fout)
 
 ### Errors
 # Token indices sequence length is longer than the specified maximum sequence length for this model (720 > 512). Running this sequence through the model will result in indexing errors
@@ -176,7 +178,7 @@ def part_3(args):
 
 
 
-def part_4(args):
+def part_4(args, nlp):
     # TODO this involves reading in ontonotes data, getting embeddings for the entities, then training a classifier with the paired embeddings and labels.
     # TODO they should then try with different combination functions and see what they think
     # take extracted representations and labels, and train a classifier
@@ -199,18 +201,25 @@ def part_4(args):
         self.classify_documents(test_items, has_labels=True)
 
 
-def main(args):
+def main(args, nlp):
     dict2func = {
         "1": part_1,
         "2": part_2,
         "3": part_3,
         "4": part_4,
     }
-    
-    dict2func[args.part](args)
+
+    dict2func[args.part](args, nlp)
 
 
 if __name__ == "__main__":
     args = setup_argparse()
 
-    main(args)
+    # validation checks that model is downloaded
+    spacy_model_name = 'en_core_web_trf'
+    if not spacy.util.is_package(spacy_model_name):
+        spacy.cli.download(spacy_model_name)
+
+    nlp = spacy.load('en_core_web_trf')
+
+    main(args, nlp)
